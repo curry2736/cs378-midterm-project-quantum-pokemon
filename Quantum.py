9f1e4718ee141e4b790e3794d71a1d5272be7e8f
curry2736 <arisrei.l.lim@gmail.com>
2022-10-08 00:03:25 -0500

    nullify and player move order switch

--- Quantum.py
+++ Quantum.py
@@ -10,29 +10,37 @@ aux = 2
 #defense = 0
 
 def fiftyPercentAtk(qc, multiplier, current_player):
-    base = 20
     atk = current_player
     defense = 1 - current_player
     qc.h(defense)
-    # qc.measure(defense, 0)
-    # counts = execute(qc, backend = Aer.get_backend('qasm_simulator'), shots=1).result().get_counts(qc)
-    # if '01' in counts:
-    #     return newAtk
-    # return 0
+
+def twentyFivePercentAtk(qc, multiplier, current_player):
+    defense = 1 - current_player
+    createUnitary(qc, 0.25, defense)
 
 def reflect(qc, probability, current_player):
     defense = current_player
     atk = 1 - current_player
     createUnitary(qc, probability, aux)
-    qc.ccx(aux, defense, atk)
-    qc.ch(aux, defense)
+    qc.cswap(aux, atk, defense)
 
-def measure(qc):
+def nullify(qc, probability, current_player, move_to_nullify):
+    if move_to_nullify == '50%':
+        createUnitary(qc, probability, aux)
+        qc.ch(aux, current_player)
+    elif move_to_nullify == '25%':
+        createUnitary(qc, probability, aux)
+        createUnitaryInverseControlled(qc, 0.25, aux, current_player)
+
+
+def measure(qc, move_history):
+    p1_move = move_history[-2]
+    p2_move = move_history[-1]
     qc.measure(range(2), range(2))
     counts = execute(qc, backend = Aer.get_backend('qasm_simulator'), shots=1).result().get_counts(qc)
     result = list(counts.keys())[0]
     qc.draw('mpl', filename='circuit.png')
-    print(result)
+    #print(result)
     #returning [p1, p2]
     return [int(result[1]), int(result[0])]
 
@@ -40,20 +48,19 @@ def measure(qc):
 #probability is probability that targetted qubit is affected
 def createUnitary(qc, probability, qubit):
     qc.u(2 * np.arcsin(probability ** 0.5),0,0,qubit)
+def createUnitaryInverseControlled(qc, probability, control, target):
+    qc.cu(2 * np.arcsin(probability ** 0.5),np.pi,np.pi,0,control, target)
 
 
 if __name__ == "__main__":
-    qc = QuantumCircuit(3,2)
-    fiftyPercentAtk(qc, 0.5)
-    reflect(qc,0.1)
+
+    qc = QuantumCircuit(3,3)
+    fiftyPercentAtk(qc, 1, 0)
+    nullify(qc, 1, 1, "50%")
+
+    qc.measure([0,1], [0,1])
     qc.draw('mpl', filename='circuit.png')
 
-# qc.h(defense)
-# fiftyPercentAtk(1.15)
-# reflect()
-# qc.measure([atk[0], defense[0]], cbits)
-# qc.draw('mpl', filename='circuit.png')
-
-# counts = execute(qc, backend = Aer.get_backend('qasm_simulator'), shots=1).result().get_counts(qc)
-# print(counts)
-# plot_histogram(counts, filename='histogram.png')
+    counts = execute(qc, backend = Aer.get_backend('qasm_simulator'), shots=1024).result().get_counts(qc)
+    print(counts)
+    plot_histogram(counts, filename='histogram.png')
--- QuantumPokemon.py
+++ QuantumPokemon.py
@@ -1,4 +1,4 @@
-from Quantum import fiftyPercentAtk, measure, reflect
+from Quantum import fiftyPercentAtk, measure, nullify, reflect, twentyFivePercentAtk
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, Aer, execute
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,13 +6,35 @@ from qiskit.visualization import plot_histogram
 from qiskit.quantum_info import Statevector
 
 
+
+move_history_map = { 
+    "50%": 20,
+    "25%": 10
+}
+
+def dmg_cnts(measure_results, move_history, first_player):
+    bases = [0, 0]
+    multipliers = []
+    multipliers.append(types[type_list[first_player]]["attack"])
+    multipliers.append(types[type_list[1 - first_player]]["attack"])
+    final_dmg = [0, 0]
+    if move_history[-1] == "reflect":
+        multipliers[0], multipliers[1] = multipliers[1], multipliers[0]
+        bases[0] = move_history_map[move_history[-2]]
+    else:
+        bases[0] = move_history_map[move_history[-1]]
+        bases[1] = move_history_map[move_history[-2]]
+    final_dmg[0] = measure_results[0] * bases[0] * multipliers[0]
+    final_dmg[1] = measure_results[1] * bases[1] * multipliers[1]
+    return final_dmg
+
 aux = 2
 atk = 1
 defense = 0
 move_count = 0
 moves = {'a': 10, 'b': 15, 'c': 20, 'd': 25}
-quantum_moves = ['f', 'r']
-move_map = {'f': fiftyPercentAtk, 'r': reflect}
+quantum_moves = ['25%', '50%', 'reflect', 'nullify']
+move_map = {'25%': twentyFivePercentAtk, '50%': fiftyPercentAtk, 'r': reflect}
 
 types = {
     "1": {
@@ -37,21 +59,20 @@ health_list = [types[type_list[0]]["health"], types[type_list[1]]["health"]]
 player = 0
 
 qc = QuantumCircuit(3, 2)
+move_history = []
 while health_list[0] > 0 and health_list[1] > 0:
     if move_count > 0 and move_count % 2 == 0:
-        base = 20
-        results = measure(qc)
+        results = measure(qc, move_history)
+        first_player = 1 - ((move_count % 4) // 2)
+        dmgs = dmg_cnts(results, move_history, first_player)
+        print("Player " + str(first_player) + " received " + str(dmgs[0]) + " damage")
+        print("Player " + str(1 - first_player) + " received " + str(dmgs[1]) + " damage")
+        health_list[first_player] -= dmgs[0]
+        health_list[1 - first_player] -= dmgs[1]
 
-        #attack_list should be the other player's attack
-        p1_dmg = results[0] * base * types[type_list[1]]["attack"]
-        p2_dmg = results[1] * base * types[type_list[0]]["attack"]
-        print("Player 1 received " + str(p1_dmg) + " damage")
-        print("Player 2 received " + str(p2_dmg) + " damage")
-        health_list[0] -= p1_dmg
-        health_list[1] -= p2_dmg
-
-            
         qc = QuantumCircuit(3,2)
+    if move_count > 0 and move_count % 4 == 0: # keeps the game fair
+        player = 1 - player
     #select moves
     print("Your health is " + str(health_list[player]))
     print("Your moves are " + str(list(moves.keys())))
@@ -65,10 +86,15 @@ while health_list[0] > 0 and health_list[1] > 0:
         move = input("Player " + str(player + 1) +  ", enter the letter for your move: ")
     
     if move in quantum_moves:
-        if move == 'f':
+        if move == '50%':
             fiftyPercentAtk(qc, types[type_list[player]]["attack"], player)
-        elif move == 'r':
+        elif move == 'reflect':
             reflect(qc, types[type_list[player]]["defense"], player)
+        elif move == '25%':
+            twentyFivePercentAtk(qc, types[type_list[player]]["attack"], player)
+        elif move == 'nullify':
+            nullify(qc, types[type_list[player]]["defense"], player, move_history[-1])
+        move_history.append(move)
     else:
         print("Your move did " + str(moves[move]) + " damage")
         health_list[player - 1] -= moves[move]
