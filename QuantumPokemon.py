74821ed23e23672fa82a6d46c346d2fe15bb7403
curry2736 <arisrei.l.lim@gmail.com>
2022-10-11 13:43:52 -0500

    fixed move history stuff

--- QuantumPokemon.py
+++ QuantumPokemon.py
@@ -2,14 +2,15 @@ from Quantum import fiftyPercentAtk, measure, nullify, reflect, twentyFivePercen
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, Aer, execute
 import numpy as np
 import matplotlib.pyplot as plt
+import PyQt6
 from qiskit.visualization import plot_histogram
 from qiskit.quantum_info import Statevector
 
 
 
 move_history_map = { 
-    "50%": 20,
-    "25%": 10
+    "50%": 10,
+    "25%": 20
 }
 
 def dmg_cnts(measure_results, move_history, first_player):
@@ -21,6 +22,8 @@ def dmg_cnts(measure_results, move_history, first_player):
     if move_history[-1] == "reflect":
         multipliers[0], multipliers[1] = multipliers[1], multipliers[0]
         bases[0] = move_history_map[move_history[-2]]
+    elif move_history[-1] == "nullify":
+        bases[1] = move_history_map[move_history[-2]]
     else:
         bases[0] = move_history_map[move_history[-1]]
         bases[1] = move_history_map[move_history[-2]]
@@ -34,23 +37,27 @@ defense = 0
 move_count = 0
 moves = {'a': 10, 'b': 15, 'c': 20, 'd': 25}
 quantum_moves = ['25%', '50%', 'reflect', 'nullify']
-move_map = {'25%': twentyFivePercentAtk, '50%': fiftyPercentAtk, 'r': reflect}
+
+# lightning strike: high chance other player gets hit, low chance you get hit 
+
+# breakout room banishment: forces you to skip a turn
 
 types = {
-    "1": {
+    "Pikachu": {
         "health": 50,
         "attack": 0.9,
-        "defense": 1
+        "defense": 0.33
     },
-    "2":  {
+    "Dr. Davis":  {
         "health": 40,
         "attack": 1.125,
-        "defense": 1
+        "defense": 0.2
     }
 }
 
 #start game
 
+print("Welcome to Quantum Pokemon!")
 type_list = [input("Player 1, select your type: "), input("Player 2, select your type: ")]
 print("-------------------------------------------------------------")
 health_list = [types[type_list[0]]["health"], types[type_list[1]]["health"]]
@@ -65,10 +72,13 @@ while health_list[0] > 0 and health_list[1] > 0:
         results = measure(qc, move_history)
         first_player = 1 - ((move_count % 4) // 2)
         dmgs = dmg_cnts(results, move_history, first_player)
-        print("Player " + str(first_player) + " received " + str(dmgs[0]) + " damage")
-        print("Player " + str(1 - first_player) + " received " + str(dmgs[1]) + " damage")
+        print("Player " + str(first_player + 1) + " received " + str(dmgs[0]) + " damage")
+        print("Player " + str((1 - first_player) + 1) + " received " + str(dmgs[1]) + " damage")
         health_list[first_player] -= dmgs[0]
         health_list[1 - first_player] -= dmgs[1]
+
+        if health_list[0] <= 0 or health_list[1] <= 0:
+            break
 
         qc = QuantumCircuit(3,2)
     if move_count > 0 and move_count % 4 == 0: # keeps the game fair
@@ -103,3 +113,10 @@ while health_list[0] > 0 and health_list[1] > 0:
 
     player = 1 - player
     move_count += 1
+    
+if (health_list[0] > 0) :
+    print("Player 2 died, Player 1 won!")
+elif (health_list[1] > 0):
+    print("Player 1 died, Player 2 won!")
+else: 
+    print("You both died, you suck lol!")
